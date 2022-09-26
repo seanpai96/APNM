64c9543da61cea788b1267ba1e05179f8848594d
SeanPai <seanpai96@gmail.com>
2022-09-26 23:05:52 +0800

    init

---
+++ src/ara/nm/nm.cpp
@@ -0,0 +1,12 @@
+
+
+namespace ara {
+    namespace nm {
+        // @brief: This function is used to initialize the Network Management.
+        // @param: None
+        // @return: None
+        void Init() {
+            
+        }
+    }  // namespace nm
+}  // namespace ara
