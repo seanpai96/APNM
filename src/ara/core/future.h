2424fc5f0d211f76bd1c864748e7f9c4d6470d79
secminhr <secminhrian@gmail.com>
2023-03-01 19:24:06 +0800

    functions should provided by skeleton

---
+++ src/ara/core/future.h
@@ -0,0 +1,9 @@
+#include "error_code.h"
+namespace ara {
+    namespace core {
+        template<typename T, typename E = ErrorCode>
+        class Future {
+            
+        };
+    }
+}
--- src/ara/nm/NetworkState_Handle1Skeleton.hpp
+++ src/ara/nm/NetworkState_Handle1Skeleton.hpp
@@ -1,12 +1,15 @@
 #include "com/fakecom.hpp"
 #include "core/result.h"
 #include "core/instance_specifier.h"
+#include "future.h"
+
 namespace ara {
     namespace nm {
         //Adapted from AUTOSAR_EXP_ARAComAPI Listing 5.12
         //The content of this class should be generated, but sadly we don't have a generator
         class NetworkState_Handle1Sketelon {
             public:
+            //constructors
             NetworkState_Handle1Sketelon(
                 ara::com::InstanceIdentifier instanceId,
                 ara::com::MethodCallProcessingMode mode = 
@@ -44,6 +47,17 @@ namespace ara {
                 ara::com::MethodCallProcessingMode mode =
                     ara::com::MethodCallProcessingMode::kEvent
             ) noexcept;
+
+            //prohibited constructor/operator
+            NetworkState_Handle1Sketelon(const NetworkState_Handle1Sketelon& other) = delete;
+            NetworkState_Handle1Sketelon& operator=(const NetworkState_Handle1Sketelon& other) = delete;
+
+            //destructor
+            ~NetworkState_Handle1Sketelon();
+
+            ara::core::Result<void> OfferService();
+            void StopOfferService();
+            ara::core::Future<bool> ProcessNextMethodCall();
         };
     }
 }
