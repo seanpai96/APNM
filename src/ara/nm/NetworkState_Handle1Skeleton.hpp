7fa418ac138c1610d0d7be2ceacf0cfc2a8be466
secminhr <secminhrian@gmail.com>
2023-03-01 19:55:08 +0800

    Skeleton declaration finished

--- src/ara/nm/NetworkState_Handle1Skeleton.hpp
+++ src/ara/nm/NetworkState_Handle1Skeleton.hpp
@@ -5,59 +5,75 @@
 
 namespace ara {
     namespace nm {
-        //Adapted from AUTOSAR_EXP_ARAComAPI Listing 5.12
-        //The content of this class should be generated, but sadly we don't have a generator
-        class NetworkState_Handle1Sketelon {
-            public:
-            //constructors
-            NetworkState_Handle1Sketelon(
-                ara::com::InstanceIdentifier instanceId,
-                ara::com::MethodCallProcessingMode mode = 
-                    ara::com::MethodCallProcessingMode::kEvent
-            );
+        namespace skeleton {
+            //Adapted from AUTOSAR_EXP_ARAComAPI Listing 5.12
+            //The content of this class should be generated, but sadly we don't have a generator
+            class NetworkState_Handle1Sketelon {
+                public:
+                //constructors
+                NetworkState_Handle1Sketelon(
+                    ara::com::InstanceIdentifier instanceId,
+                    ara::com::MethodCallProcessingMode mode = 
+                        ara::com::MethodCallProcessingMode::kEvent
+                );
 
-            static ara::core::Result<NetworkState_Handle1Sketelon> Create(
-                const ara::com::InstanceIdentifier &instanceID,
-                ara::com::MethodCallProcessingMode mode =
-                    ara::com::MethodCallProcessingMode::kEvent
-            ) noexcept;
-
-
-            NetworkState_Handle1Sketelon(
-                ara::com::InstanceIdentifierContainer instanceIds,
-                ara::com::MethodCallProcessingMode mode =
-                    ara::com::MethodCallProcessingMode::kEvent
-            );
-
-            static ara::core::Result<NetworkState_Handle1Sketelon> Create(
-                const ara::com::InstanceIdentifierContainer &instanceIDs,
-                ara::com::MethodCallProcessingMode mode =
-                    ara::com::MethodCallProcessingMode::kEvent
-            ) noexcept;
+                static ara::core::Result<NetworkState_Handle1Sketelon> Create(
+                    const ara::com::InstanceIdentifier &instanceID,
+                    ara::com::MethodCallProcessingMode mode =
+                        ara::com::MethodCallProcessingMode::kEvent
+                ) noexcept;
 
 
-            NetworkState_Handle1Sketelon(
-                ara::core::InstanceSpecifier instanceSpec,
-                ara::com::MethodCallProcessingMode mode =
-                    ara::com::MethodCallProcessingMode::kEvent
-            );
+                NetworkState_Handle1Sketelon(
+                    ara::com::InstanceIdentifierContainer instanceIds,
+                    ara::com::MethodCallProcessingMode mode =
+                        ara::com::MethodCallProcessingMode::kEvent
+                );
 
-            static ara::core::Result<NetworkState_Handle1Sketelon> Create(
-                const ara::core::InstanceSpecifier &instanceSpec,
-                ara::com::MethodCallProcessingMode mode =
-                    ara::com::MethodCallProcessingMode::kEvent
-            ) noexcept;
+                static ara::core::Result<NetworkState_Handle1Sketelon> Create(
+                    const ara::com::InstanceIdentifierContainer &instanceIDs,
+                    ara::com::MethodCallProcessingMode mode =
+                        ara::com::MethodCallProcessingMode::kEvent
+                ) noexcept;
 
-            //prohibited constructor/operator
-            NetworkState_Handle1Sketelon(const NetworkState_Handle1Sketelon& other) = delete;
-            NetworkState_Handle1Sketelon& operator=(const NetworkState_Handle1Sketelon& other) = delete;
 
-            //destructor
-            ~NetworkState_Handle1Sketelon();
+                NetworkState_Handle1Sketelon(
+                    ara::core::InstanceSpecifier instanceSpec,
+                    ara::com::MethodCallProcessingMode mode =
+                        ara::com::MethodCallProcessingMode::kEvent
+                );
 
-            ara::core::Result<void> OfferService();
-            void StopOfferService();
-            ara::core::Future<bool> ProcessNextMethodCall();
-        };
+                static ara::core::Result<NetworkState_Handle1Sketelon> Create(
+                    const ara::core::InstanceSpecifier &instanceSpec,
+                    ara::com::MethodCallProcessingMode mode =
+                        ara::com::MethodCallProcessingMode::kEvent
+                ) noexcept;
+
+                //prohibited constructor/operator
+                NetworkState_Handle1Sketelon(const NetworkState_Handle1Sketelon& other) = delete;
+                NetworkState_Handle1Sketelon& operator=(const NetworkState_Handle1Sketelon& other) = delete;
+
+                //destructor
+                ~NetworkState_Handle1Sketelon();
+
+                ara::core::Result<void> OfferService();
+                void StopOfferService();
+                ara::core::Future<bool> ProcessNextMethodCall();
+
+                fields::NetworkCurrentState NetworkCurrentState;
+                fields::NetworkRequestedState NetworkRequestedState;
+            };
+
+            //fields
+            namespace fields {
+                class NetworkCurrentState {
+
+                };
+
+                class NetworkRequestedState {
+
+                };
+            }
+        }
     }
 }
