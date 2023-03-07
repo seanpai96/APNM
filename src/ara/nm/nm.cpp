a596def99d7b7b2921f02e111c21fed007d05867
Sean <seanpai96@gmail.com>
2023-03-08 03:17:13 +0800

    Basic functions of NMInstance + TImer

--- src/ara/nm/nm.cpp
+++ src/ara/nm/nm.cpp
@@ -1,12 +1,38 @@
+#include "nm.hpp"
 
+#include <bits/stdc++.h>
+
+#include "CallbackTimer.cpp"
 
 namespace ara {
     namespace nm {
-        // @brief: This function is used to initialize the Network Management.
-        // @param: None
-        // @return: None
-        void Init() {
-            
+        class NMInstance {
+            NMNetworkHandle &handle;
+            UdpNmNode &node;
+            UdpNmCluster &cluster;
+            CallbackTimer timer;
+
+           public:
+            NMInstance(NMNetworkHandle &handle, UdpNmNode &node, UdpNmCluster &cluster);
+            void StartInstance();
+            void StopInstance();
+            void Tick();
+
+           private:
+            int _ticks = 0;
+        };
+
+        NMInstance::NMInstance(NMNetworkHandle &handle, UdpNmNode &node,
+                               UdpNmCluster &cluster)
+            : handle(handle), node(node), cluster(cluster) {}
+
+        void NMInstance::StartInstance() {
+            timer.start(1000, [this]() { this->Tick(); });
+        }
+
+        void NMInstance::StopInstance() { timer.stop(); }
+
+        void NMInstance::Tick() {
         }
     }  // namespace nm
 }  // namespace ara
