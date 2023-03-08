4a6f6e522eeb27d8d8c665c05e2f86e7f75529e8
secminhr <secminhrian@gmail.com>
2023-03-08 16:16:26 +0800

    StateMachine interface

---
+++ src/ara/nm/IStateMachine.hpp
@@ -0,0 +1,11 @@
+#ifndef ARA_NM_I_STATE_MACHINE_H_
+#define ARA_NM_I_STATE_MACHINE_H_
+
+#include <functional>
+
+class IStateMachine {
+    virtual void start(std::function<void(bool)> onStateChangeToNetwork);
+    virtual void setRequested(bool requested);
+};
+
+#endif
