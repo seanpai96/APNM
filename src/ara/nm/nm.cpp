2a7c302b335fc28b101189f054ab346e4eab87d7
Sean <seanpai96@gmail.com>
2023-03-12 21:16:39 +0800

    move nmInstance class to header

--- src/ara/nm/nm.cpp
+++ src/ara/nm/nm.cpp
@@ -5,50 +5,8 @@
 
 //#include <bits/stdc++.h>
 
-#include "../../../include/prototype.h"
-#include "../../../socket/socket.cpp"
-#include "CallbackTimer.cpp"
-#include "IStateMachine.hpp"
-#include "NetworkStateType.hpp"
-
 namespace ara {
     namespace nm {
-        class NMInstance : public IStateMachine {
-            UdpNmNode &node;
-            UdpNmCluster &cluster;
-            CallbackTimer timer;
-            NMInstanceState state = NMInstanceState::NM_STATE_INIT;
-            std::function<void(bool)> &onStateChangeToNetwork;
-            Socket socket;
-
-           public:
-            NMInstance(UdpNmNode &node, UdpNmCluster &cluster, std::function<void(bool)> &onStateChangeToNetwork);
-            void StartInstance();
-            void StopInstance();
-            void setRequested(bool requested);
-            void Reset();
-            NMInstanceState GetState() { return state; }
-
-           private:
-            int _ticks = 0;
-            bool isNetworkRequested = false;
-            bool recievedNmMsg = false;
-            bool isNmTimeoutTimerRunning = false;
-            bool isNmRepeatMessageTimerRunning = false;
-            bool isNmMsgCycleTimerRunning = false;
-            bool isNmImmediateCycleTimerRunning = false;
-            bool isNmWaitBusSleepTimerRunning = false;
-            int nmMsgCycleOffsetRemainingTicks = 0;
-            int nmTimeoutTimerTicks = 0;
-            int nmRepeatMessageTimerTicks = 0;
-            int nmMsgCycleTimerTicks = 0;
-            int nmImmediateCycleTimerTicks = 0;
-            int immediateCycleTimes = 0;
-            int nmWaitBusSleepTimerTicks = 0;
-            void RestartTimeoutTimer();
-            void Tick();
-        };
-
         NMInstance::NMInstance(UdpNmNode &node, UdpNmCluster &cluster, std::function<void(bool)> &onStateChangeToNetwork)
             : node(node), cluster(cluster), onStateChangeToNetwork(onStateChangeToNetwork) {
             socket.setServerAndBind();
